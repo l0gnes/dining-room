973f238a5ae7c79b49efc7b7a7ce53856893b27e
l0gnes <loonargt@gmail.com>
2022-03-18 19:37:26 -0300

    Server intial commit

---
+++ server/routes/recipes.js
@@ -0,0 +1,136 @@
+/*
+* All endpoints in this file should be prefixed with /recipes according to `server.js`
+*/
+
+const express = require("express");
+const router = express.Router();
+
+const { PrismaClient, RecordNotFound } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+/*  /recipes/all
+*       GET: Gets all of the existing recipes, with pagination!     @CRUD:R    
+*/
+
+router.get(
+    "/all",
+    async (req, res) => {
+
+        let recipesList;
+
+        if(!req.query.search)
+        {
+
+            // If there is no search query then there is no need to add a where clause i guess
+
+            recipesList = await prisma.recipe.findMany(
+                {
+                    skip: 25 * (req.query.page | 0),
+                    take: 25
+                }
+            );
+
+        }
+        else // TODO(@l0gnes): https://github.com/l0gnes/dining-room/issues/13
+        {
+
+            recipesList = await prisma.recipe.findMany(
+                {
+                    skip: 25 * (req.query.page | 0),
+                    take: 25,
+                    where : {
+                        name : {
+                            contains : req.query.search,
+                            mode: "insensitive"             // Removes case-sensitive searches
+                        }
+                    }
+                }
+            );
+
+        }
+
+        console.log("recipesList: " + recipesList);
+
+        res.json(
+            recipesList
+        );
+
+    }
+);
+
+/*  /recipes/:id
+*       GET: Fetch a single recipe                                  @CRUD:R
+*       PUT: Updates a specific recipe with new data                @CRUD:U
+*       DELETE: You'll never guess what this does                   @CRUD:D
+*/
+
+// TODO(@l0gnes): Magic Tags as per issue https://github.com/l0gnes/dining-room/issues/13
+// @l0gnes : Closes issue https://github.com/l0gnes/dining-room/issues/4
+
+router.get(
+    "/:id",
+    async (req, res) => {
+
+        let singleRecipe = await prisma.recipe.findUnique(
+            {
+                where: {
+                    id: parseInt(req.params.id)
+                }
+            }
+        );
+
+        return res.json(
+            singleRecipe
+        );
+
+    }
+);
+
+// @l0gnes : Closes issue https://github.com/l0gnes/dining-room/issues/2
+
+router.delete(
+    "/:id",
+    async (req, res) => {
+
+        try {
+
+            let deletedRecipe = await prisma.recipe.delete(
+                {
+                    where: {
+                        id: parseInt(req.params.id)
+                    }
+                }
+            );
+
+        } catch (RecordNotFound) {
+            return res.send({});
+        }
+
+        return res.json(
+            deletedRecipe
+        );
+
+    }    
+)
+
+/*
+*   /recipes
+*       POST: Creates a new instance of recipe from a form
+*/
+
+// @l0gnes : Closes issue https://github.com/l0gnes/dining-room/issues/1
+
+router.post(
+    "/",
+    async (req, res) => {
+        res.json(
+            {
+                "body" : req.body,
+                "file" : req.file
+            }
+        )
+    }
+)
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const bodyparser = require("body-parser");
+const cors = require('cors');
+const multer = require('multer');
 const app = express();
 
-const testEndpoints = require('./routes/test');
+app.use(cors(
+    {
+        origin: "*",                // I'm aware this is insecure but docker moment
+        optionsSuccessStatus: 200
+    }
+));
 
+app.use(bodyparser.json());                         // Parses JSON data
+app.use(bodyparser.urlencoded({ extended: true}));  // Parses application/www
+
+upload = multer();
+app.use(upload.array());                            // For parsing multipart data
+
+const testEndpoints = require('./routes/test');
 app.use('/test', testEndpoints);
+
+const recipeAPIEndpoints = require('./routes/recipes');
+app.use('/recipes', recipeAPIEndpoints);
+
+// For all those epic sandwich recipes
+app.use('/static', express.static("food_images"));
 
 app.listen(5000);
