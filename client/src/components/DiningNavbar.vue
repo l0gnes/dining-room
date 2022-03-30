27df8ab44aea39ce069ae3c7e793f56987773cc3
l0gnes <loonargt@gmail.com>
2022-03-29 22:46:36 -0300

    Fixed the navbar opacity issue

--- client/src/components/DiningNavbar.vue
+++ client/src/components/DiningNavbar.vue
@@ -1,12 +1,12 @@
 <template>
-  <b-navbar toggleable="lg" id="dining-navbar-base" class="text-white" type="light">
+  <b-navbar toggleable="lg" id="dining-navbar-base" type="dark">
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item to="" active-class="active">Recipes</b-nav-item>
-        <b-nav-item to="about" active-class="active">About</b-nav-item>
+        <b-nav-item class="dining-nav-link" to="" active-class="active">Recipes</b-nav-item>
+        <b-nav-item class="dining-nav-link" to="about" active-class="active">About</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
 
@@ -19,26 +19,30 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
   #dining-navbar-base {
     position: absolute;
     background: rgba(0,0,0,0);
+  }
 
-    .nav-link {
+  .dining-nav-link {
       color: white !important;
       padding: 0.5rem 1rem;
-      opacity: 70%;
+      opacity: 0.7;
       font-weight: 500;
       transition: opacity 0.25s ease-in-out;
       margin-right: 1rem;
+      border-color: white;
+      transition: border 0.1s ease;
+      border-bottom-style: solid;
+      border-bottom-width: 0;
 
       &:hover {
-        opacity: 100%;
+        opacity: 1;
+        border-bottom-width: 5px;
       }
 
     }
-
-  }
 
 </style>
